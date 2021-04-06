6e75329a4ff26b0deec29f350b89d74f7f4e3f22
O-Julia-O <julia.qq.onix@gmail.com>
2021-04-06 15:30:33 +0300

    Lambda 2

--- functions/functionLambda.js
+++ functions/functionLambda.js
@@ -6,3 +6,28 @@ let welcome = (age > 18) ?
 
 welcome(age)
 
+/* Перепишите с использованием функции-стрелки
+Замените код Function Expression стрелочной функцией: */
+
+/* 
+function ask(question, yes, no) {
+    if (confirm(question)) yes()
+    else no();
+}
+
+ask(
+  "Вы согласны?",
+  function() { alert("Вы согласились."); },
+  function() { alert("Вы отменили выполнение."); }
+); */
+
+let ask = (question, yes, no) => {
+    if (question) {
+        yes()
+    }
+    else {
+        no()
+    }
+}
+
+ask(true, () => console.log("Вы согласились"), () => console.log("Вы отменили выполнение"))
