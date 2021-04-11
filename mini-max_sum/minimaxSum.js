2b93819d4c1f631821ffaecd8a5680ec3a7656ae
O-Julia-O <julia.qq.onix@gmail.com>
2021-04-12 00:52:21 +0300

    Initial commit

---
+++ mini-max_sum/minimaxSum.js
@@ -0,0 +1,26 @@
+/*
+    Given five positive integers, find the minimum and maximum 
+    values that can be calculated by summing exactly four of the five integers. 
+    Then print the respective minimum and maximum values as a single line of 
+    two space-separated long integers. 
+
+    comments
+    ! comment RED
+    * comment GREEN
+    & ROSE COMMENTS
+    ^ YELLOW COMMENTS
+    ~ PURPLE COMMENTS
+    ? BLUE COMMENTS
+    TODO colorful comments ORANGE *-*
+
+    anchors
+    LINK 
+    SECTION
+    ANCHOR 
+    REVIEW
+    NOTE
+    FIXME
+    STUB
+*/
+
+
