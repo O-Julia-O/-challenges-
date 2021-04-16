db8ec22530266408cd9d5236c668aa3b4b4354d5
O-Julia-O <julia.qq.onix@gmail.com>
2021-04-16 03:01:43 +0300

    studying objects..

---
+++ objects/test3.js
@@ -0,0 +1,17 @@
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
+
+function sumSalaries(salaries) {
+    let sum = 0;
+
+    for (let key in salaries) {
+        sum += salaries[key];
+    }
+
+    return sum;
+}
+
+console.log(sumSalaries(salaries));
